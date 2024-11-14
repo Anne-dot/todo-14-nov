392b18c003badef933277a1720cbac99aee23c96
Anne-Dot <ruusmann@gmail.com>
2024-11-14 17:39:01 +0200

    initial

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,8 +1,10 @@
-import { PrismaClient, TodoStatus } from "@prisma/client";
+import { TodoStatus } from "@prisma/client";
+import prisma from "../config/prisma.js";
 
-const prisma = new PrismaClient()
 
 export const getAllTodos = async (request, response) => {
+
+    console.log(request);
 
     try {
         const todos = await prisma.todo.findMany();
@@ -41,4 +43,75 @@ export const createTodo = async (request, response) => {
         })
     }
 
+}
+
+export const getTodo = async (request, response) => {
+
+    try {
+        const { id } = request.params
+
+    const todo = await prisma.todo.findUnique({
+        where: {
+            id: Number(id)
+        }
+    })
+    
+    response.status(200).json(todo)
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({
+            message: "Bad luck. Try again."
+        })
+    }
+}
+
+export const deleteTodo = async (request, response) => {
+
+    try {
+        const { id } = request.params
+
+    await prisma.todo.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+    
+    response.status(200).json({
+        message: "Deleted successfully."
+    })
+
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({
+            message: "Bad luck. Try again."
+        })
+    }
+}
+
+export const updateTodo = async (request, response) => {
+
+    try {
+        const { id } = request.params
+        const { title, description } = request.body
+
+        const updatedTodo = await prisma.todo.update({
+        where: {
+            id: Number(id)
+        },
+        data: {
+            title,
+            description
+        }
+    })
+    
+    response.status(200).json({
+        message: "Todo updated successfully.",
+        updatedTodo
+    })
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({
+            message: "Bad luck. Try again."
+        })
+    }
 }
